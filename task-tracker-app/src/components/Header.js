9cd0cf1bbfcee5708d049b28bab10a24226dbd56
Vihanga Malinda Fernando <vihangamalindafernando@gmail.com>
2021-09-12 08:21:01 -0800

    Initial state of the application.

---
+++ task-tracker-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Button from './Button'
+
+const Header = ({title}) => {
+    return (
+        <header className="header">
+            <h1>{title}</h1>
+            <Button text="Add"/>
+        </header>
+    )
+}
+
+export default Header
